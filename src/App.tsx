a64bfcdbd8229a8ae21310b622c63aeafa2887e4
Mark Marszal <mark@faultycode.com>
2023-06-14 15:51:36 +0200

    added initial AppShell

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,68 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { 
+  AppShell, 
+  Navbar, 
+  Header,
+  Text,
+  MediaQuery,
+  Burger,
+  useMantineTheme,
+  MantineProvider,
+  ColorScheme,
+  ColorSchemeProvider
+} from '@mantine/core';
+import {
+  useHotkeys,
+  useLocalStorage
+} from '@mantine/hooks';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const theme = useMantineTheme();
+  const [opened, setOpened] = useState(false);
+
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+    key: 'mantine-color-scheme',
+    defaultValue: 'dark',
+    getInitialValueInEffect: true,
+  });
+
+  useHotkeys([['mod+J', () => toggleColorScheme()]]);
+
+  const toggleColorScheme = (value?: ColorScheme) => setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <MantineProvider theme={{colorScheme}} withNormalizeCSS withGlobalStyles>
+        <AppShell
+          navbarOffsetBreakpoint="sm"
+          asideOffsetBreakpoint="sm"
+          navbar={
+            <Navbar p="md" hiddenBreakpoint="sm" hidden={!opened} width={{ sm: 200, lg: 300 }}>
+              <Text>Application navbar</Text>
+            </Navbar>
+          }
+          header={
+            <Header height={{ base: 50, md: 70 }} p="md">
+              <div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
+                <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
+                  <Burger
+                    opened={opened}
+                    onClick={() => setOpened((o) => !o)}
+                    size="sm"
+                    color={theme.colors.gray[6]}
+                    mr="xl"
+                  />
+                </MediaQuery>
+
+                <Text>Application header</Text>
+              </div>
+            </Header>
+          }
+        >
+          <Text>Resize app to see responsive navbar in action</Text>
+        </AppShell>
+      </MantineProvider>
+    </ColorSchemeProvider>
   )
 }
 
