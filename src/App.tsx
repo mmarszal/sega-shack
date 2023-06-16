7d8e37e9630d88b3ce81ea222253ac541f63001e
Mark Marszal <mark@faultycode.com>
2023-06-16 13:03:13 +0200

    added initial shell scheme

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,18 @@ import {
   useMantineTheme,
   MantineProvider,
   ColorScheme,
-  ColorSchemeProvider
+  ColorSchemeProvider,
+  ActionIcon,
+  Group,
+  ScrollArea,
+  NavLink
 } from '@mantine/core';
 import {
   useHotkeys,
   useLocalStorage
 } from '@mantine/hooks';
+import { IconSun, IconMoonStars, IconDeviceGamepad, IconHome } from '@tabler/icons-react';
+import Logo from './components/Logo';
 
 function App() {
   const theme = useMantineTheme();
@@ -38,24 +44,54 @@ function App() {
           asideOffsetBreakpoint="sm"
           navbar={
             <Navbar p="md" hiddenBreakpoint="sm" hidden={!opened} width={{ sm: 200, lg: 300 }}>
-              <Text>Application navbar</Text>
+              <Navbar.Section grow component={ScrollArea}>
+                <NavLink label="Home" icon={<IconHome />} />
+                <NavLink 
+                  label="Mega Drive Mods"
+                  icon={<IconDeviceGamepad size="1.5rem"/>}
+                >
+                  <NavLink label="Region Mod"/>
+                  <NavLink label="Mega Amp" />
+                  <NavLink label="NTSC/PAL Switcher" />
+                </NavLink>
+                <NavLink 
+                  label="Sega CD Mods"
+                  icon={<IconDeviceGamepad size="1.5rem"/>}
+                >
+                  <NavLink label="Region Free Bios" />
+                  <NavLink label="Bios Switcher" />
+                  <NavLink label="Restore Model 1 Sound" />
+                </NavLink>
+                <NavLink 
+                  label="Sega Saturn Mods"
+                  icon={<IconDeviceGamepad size="1.5rem"/>}
+                >
+                  <NavLink label="FRAM Replacement"/>
+                  <NavLink label="Region Free Bios" />
+                  <NavLink label="Mod Chip Install" />
+                </NavLink>
+              </Navbar.Section>
             </Navbar>
           }
           header={
-            <Header height={{ base: 50, md: 70 }} p="md">
-              <div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
-                <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
-                  <Burger
-                    opened={opened}
-                    onClick={() => setOpened((o) => !o)}
-                    size="sm"
-                    color={theme.colors.gray[6]}
-                    mr="xl"
-                  />
-                </MediaQuery>
-
-                <Text>Application header</Text>
-              </div>
+            <Header height={{ base: 60, md: 60 }} p="md">
+              <Group position="apart">
+                <Group>
+                  <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
+                    <Burger
+                      opened={opened}
+                      onClick={() => setOpened((o) => !o)}
+                      size="sm"
+                      color={theme.colors.gray[6]}
+                      mr="xl"
+                    />
+                  </MediaQuery>
+                  <Logo />
+                </Group>
+                <ActionIcon variant="default" onClick={() => toggleColorScheme()} size={30}>
+                  {colorScheme === 'dark' ? <IconSun size="1rem" /> : <IconMoonStars size="1rem" />}
+                </ActionIcon>
+              </Group>
             </Header>
           }
         >
